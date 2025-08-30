bf483dea7836f1509a7db7f687d86a7807e0d4af
Folgue02 <davidfolru@gmail.com>
2025-08-30 18:39:09 +0200

    Run failsafe handlers + Logging
    - System.out.print's replaced with actual logging.
    - FIX: Internal error route handler doesn't get executed when the
      matched handler fails.

--- src/main/java/io/github/folgue02/sapphire/server/SunSapphireServer.java
+++ src/main/java/io/github/folgue02/sapphire/server/SunSapphireServer.java
@@ -12,6 +12,7 @@ import io.github.folgue02.sapphire.router.Router;
 import io.github.folgue02.sapphire.router.handler.RouteHandler;
 import io.github.folgue02.sapphire.server.exception.ResponseWriteException;
 import io.github.folgue02.sapphire.utils.HttpUtils;
+import io.github.folgue02.sapphire.utils.RouteUtils;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
@@ -19,7 +20,11 @@ import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class SunSapphireServer extends SapphireServer implements HttpHandler {
+	private final Logger log = LoggerFactory.getLogger(getClass());
 
 	public SunSapphireServer(Router router, InetSocketAddress address) {
 		super(router, address);
@@ -27,41 +32,51 @@ public class SunSapphireServer extends SapphireServer implements HttpHandler {
 
 	@Override
 	public void run() throws Exception {
-		var server = HttpServer.create(this.address, 0);
-
-		server.createContext("/", this);
-
-		server.start();
+		try {
+			var server = HttpServer.create(this.address, 0);
+			
+			server.createContext("/", this);
+	
+			log.info("Starting server on {}", this.address);
+			server.start();
+			log.info("Server started on {}", this.address);
+		} catch (Exception e) {
+			log.error("Error while starting server.", e);
+			throw e;
+		}
 	}
 
 	@Override
 	public void handle(HttpExchange exchange) {
-		final String path = exchange.getHttpContext().getPath();
 		final RouteSpecification rSpec;
 		final RouteHandler routeHandler;
 
+		// --- Read data from request ---
 		final HttpRequest request;
 		final HttpResponse response = new HttpResponse();
 		try {
 			request = HttpUtils.requestFromExchange(exchange);
 		} catch (Exception e) {
-			System.out.println("Couldn't read from request.");
-			e.printStackTrace();
+			log.error("Couldn't read from request.", e);
 			return;
 		}
 
-		List<RouteFilter> matchedRouteFilters = this.router.findMatchedFilters(request);
-
+		
+		// --- Filter execution ---
 		// If a filter corresponds to the giving request, it is executed.
 		// If the filter decides to forward the request to a different handler, this one is 
 		// executed, and whatever other handler that would have been chosen for the request is ignored.
+		List<RouteFilter> matchedRouteFilters = this.router.findMatchedFilters(request);
+
 		for (var filter : matchedRouteFilters) {
+
 			boolean continueChain = runFilter(request, response, exchange, filter);
 			if (!continueChain)
 				return;
 		}
 
-		// Run handler
+		// --- Get handler ---
+		// If no handler is mapped for this request, use the this.noMappingFoundHandler
 		Optional<Map.Entry<RouteSpecification, RouteHandler>> handlerOpt = this.router.findMatchedRoute(request);
 		if (handlerOpt.isEmpty()) {
 			routeHandler = this.noMappingFoundHandler;
@@ -70,27 +85,38 @@ public class SunSapphireServer extends SapphireServer implements HttpHandler {
 			rSpec = handlerOpt.get().getKey();
 			routeHandler = handlerOpt.get().getValue();
 		}
-
-		if (!runHandler(request, response, exchange, routeHandler)) {
-			System.err.printf("The route handler (%s) has failed.\n", routeHandler);
-		}
+		
+		request.pathParams = RouteUtils.extractPathVariables(rSpec.routeSpecification(), request.requestUri.getPath());
+		
+		// --- Run handler --
+		runHandler(request, response, exchange, routeHandler);
 	}
-
+	
 	/// Runs the given handler, and writes to the response.
 	///
 	/// @param request Request object
 	/// @param exchange Http exchange object in which the response will be written into.
 	/// @param routeHandler Route handler to run.
-	/// @return `true` if the handler has been executed without errors, `false` otherwise.
-	public boolean runHandler(HttpRequest request, HttpResponse response, HttpExchange exchange, RouteHandler routeHandler) {
-		try (exchange) {
+	public void runHandler(HttpRequest request, HttpResponse response, HttpExchange exchange, RouteHandler routeHandler) {
+		// Run handler, and if it fails, run the failsafe.
+		try {
 			response = routeHandler.runHandler(request, response);
-			this.writeResponse(response, exchange);
-			return true;
 		} catch (Exception e) {
-			e.printStackTrace();
-			System.out.println("Couldn't handle request and write to response.");
-			return false;
+			log.error("Handler '{}' failed.", routeHandler, e);
+			if (this.internalServerErrorHandler != null) {
+				try {
+					response = this.internalServerErrorHandler.runHandler(request, new HttpResponse());
+				} catch (Exception ie) {
+					log.error("The matched request handler has failed, and when executing the internal server error handler, this one failed too. {}", ie.getMessage(), ie);
+				}
+			}
+		}
+
+		// Write response into the exchange object
+		try (exchange) {
+			this.writeResponse(response, exchange);
+		} catch (Exception e) {
+			log.error("Couldn't write the response into the exchange object. {}", e.getMessage(), e);
 		}
 	}
 
@@ -110,8 +136,7 @@ public class SunSapphireServer extends SapphireServer implements HttpHandler {
 		try {
 			forcedHandler = filter.filter(request, response);
 		} catch (Exception e) {
-			System.err.printf("An error ocurred while running the given filter(%s). \n", filter);
-			e.printStackTrace();
+			log.error("An error ocurred while running the given filter({}). ", filter, e);
 			return false;
 		}
 		
@@ -120,8 +145,7 @@ public class SunSapphireServer extends SapphireServer implements HttpHandler {
 				response = forcedHandler.get().runHandler(request, response);
 				writeResponse(response, exchange);
 			} catch (Exception e) {
-				System.err.printf("An error has ocurred while executing the handler (%s) given by the filter.", forcedHandler.get());
-				e.printStackTrace();
+				log.error("An error has ocurred while executing the handler ({}) given by the filter.", forcedHandler.get(), e);
 			}
 			return false;
 		}
